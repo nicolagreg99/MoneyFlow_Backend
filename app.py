af3813d23933e1938f0f0ae3289f06055f5d3ad7
Nicola <nicola.greggio@inkwelldata.com>
2025-03-02 09:27:39 +0100

    Add https

--- app.py
+++ app.py
@@ -218,4 +218,4 @@ def suggestions():
     return get_suggestions()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
